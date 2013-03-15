d7e11b92e2221f978e86aed29e8221e486bc4eec
Lee Whitney <lee@whitneyland.com>
2013-03-15 16:57:07 -0500

    Cleanup.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module('sportsSideApp', ['parseResource','ui', 'ui.bootstrap', 'ngGrid']);
+var app = angular.module('pApp', ['parseResource','ui', 'ui.bootstrap', 'ngGrid']);
 
 app.config(function($routeProvider) {
     $routeProvider.
@@ -15,49 +15,12 @@ app.controller('frameCtrl', frameCtrl);
 app.factory('Parse', function ($parseResource) { return $parseResource(); });
 app.service('App', appService);
 
-app.directive('dndList', function() {
-    return function(scope, element, attrs) {
 
-        // variables used for dnd
-        var toUpdate;
-        var startIndex = -1;
-
-        // watch the model, so we always know what element
-        // is at a specific position
-        scope.$watch(attrs.dndList, function(value) {
-            toUpdate = value;
-        },true);
-
-        // use jquery to make the element sortable (dnd). This is called
-        // when the element is rendered
-        $(element[0]).sortable({
-            items:'li',
-            start:function (event, ui) {
-                // on start we define where the item is dragged from
-                startIndex = ($(ui.item).index());
-            },
-            stop:function (event, ui) {
-                // on stop we determine the new index of the
-                // item and store it there
-                var newIndex = ($(ui.item).index());
-                var toMove = toUpdate[startIndex];
-                toUpdate.splice(startIndex,1);
-                toUpdate.splice(newIndex,0,toMove);
-
-                // we move items in the array, if we want
-                // to trigger an update in angular use $apply()
-                // since we're outside angulars lifecycle
-                scope.$apply(scope.model);
-            },
-            axis:'y'
-        })
-    }
-});
-
+// To use the angularjs $resource layer, add parse.com keys here
 app.constant("PARSE_CONFIG",{
         defaultHeaders: {
-            "X-Parse-Application-Id" : "VylSzcFwtgPW5P6YQAFyUbbSTM6rxYOzAcHgcHUr",
-            "X-Parse-REST-API-Key" : "OIVfUmElXCfG7nAQDfkgY2XTbzpqfgnucq0Q5yPZ"
+            "X-Parse-Application-Id" : "Your App Id Here",
+            "X-Parse-REST-API-Key" : "Your REST api key here"
         },
         defaultParams: {}
     }
